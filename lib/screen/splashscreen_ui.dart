981dbaeb196f7c10041497d295ec6635c29dc5e4
Unique-chiemerie <lilanimate1@gmail.com>
2023-10-21 11:55:07 +0100

    pulsating effect

--- lib/screen/splashscreen_ui.dart
+++ lib/screen/splashscreen_ui.dart
@@ -8,6 +8,27 @@ class Splashui extends StatefulWidget {
 }
 
 class _SplashuiState extends State<Splashui> {
+  bool showlogo = true;
+  @override
+  void initState() {
+    super.initState();
+    startanimation();
+  }
+
+  void startanimation() async {
+    await Future.delayed(
+      const Duration(milliseconds: 500),
+    );
+    while (true) {
+      await Future.delayed(
+        const Duration(seconds: 1),
+      );
+      setState(() {
+        showlogo = !showlogo;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,11 +37,24 @@ class _SplashuiState extends State<Splashui> {
           Column(
             children: [
               //the children for the splashscreen goes here
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.network(
-                    'https://img.icons8.com/?size=160&id=udMvpkRHbzzS&format=png'),
+              Center(
+                child: TweenAnimationBuilder(
+                  tween: Tween<double>(begin: 0, end: 1),
+                  duration: const Duration(seconds: 1),
+                  builder: (context, value, child) {
+                    return AnimatedOpacity(
+                      opacity: showlogo ? value : 0.0,
+                      duration: const Duration(milliseconds: 1),
+                      child: child,
+                    );
+                  },
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Image.network(
+                        'https://img.icons8.com/?size=160&id=udMvpkRHbzzS&format=png'),
+                  ),
+                ),
               )
             ],
           ),
